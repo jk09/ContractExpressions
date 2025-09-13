af3c2d6b0e273142c8a82dea0cca68176c6da92e
Jozef Košík <jozef.kosik@radixal.net>
2025-09-13 21:09:13 +0200

    Enhance Dbc and ListContracts with contract validation and old value tracking

--- ContractExpressions/ContractExpressions.cs
+++ ContractExpressions/ContractExpressions.cs
@@ -1,10 +1,73 @@
 #define CONTRACTS_FULL
 
-using System.Collections;
-using System.Diagnostics.Contracts;
-using System.Linq.Expressions;
+var listc = new ListContracts() as IList;
 
-namespace ContractExpressions;
+Dbc.Def(static (IList x, object a) => x.Add(a),
+        static (IList x, object a) => Contract.Requires(a is string ? !string.IsNullOrEmpty(a as string) : a != null),
+        static (IList x, object a) => Contract.Ensures(Contract.Result<int>() > 0 && x.Count > Contract.OldValue<int>(x.Count)));
+
+class ListContracts : IList
+{
+    object? IList.this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    bool IList.IsFixedSize => throw new NotImplementedException();
+
+    bool IList.IsReadOnly => throw new NotImplementedException();
+
+    int ICollection.Count => throw new NotImplementedException();
+
+    bool ICollection.IsSynchronized => throw new NotImplementedException();
+
+    object ICollection.SyncRoot => throw new NotImplementedException();
+
+    int IList.Add(object? value1)
+    {
+        Contract.Requires(value1 != null);
+        Contract.Ensures(Contract.Result<int>() > 0);
+
+        return 0;
+    }
+
+    void IList.Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    bool IList.Contains(object? value)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ICollection.CopyTo(Array array, int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    int IList.IndexOf(object? value)
+    {
+        throw new NotImplementedException();
+    }
+
+    void IList.Insert(int index, object? value)
+    {
+        throw new NotImplementedException();
+    }
+
+    void IList.Remove(object? value)
+    {
+        throw new NotImplementedException();
+    }
+
+    void IList.RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 static class Dbc
 {
@@ -12,7 +75,7 @@ static class Dbc
     {
         foreach (var contract in contracts)
         {
-            var visitor = new DbcDefVisitor();
+            var visitor = new DbcDefVisitor(typeof(TContract));
             visitor.Visit(contract);
 
         }
@@ -33,7 +96,7 @@ class ContractResultVisitor : ExpressionVisitor
 
 class ContractOldValueVisitor : ExpressionVisitor
 {
-    public List<MemberInfo> OldValueMembers { get; } = new();
+    public List<PropertyInfo> OldValueProperties { get; } = new();
 
     protected override Expression VisitMethodCall(MethodCallExpression node)
     {
@@ -43,10 +106,9 @@ class ContractOldValueVisitor : ExpressionVisitor
 
             var valueExpr = node.Arguments[0];
 
-            if (valueExpr is MemberExpression oldValueMemberExpr)
+            if (valueExpr is MemberExpression oldValueMemberExpr && oldValueMemberExpr.Member is PropertyInfo propertyInfo)
             {
-                var oldValueMember = oldValueMemberExpr.Member;
-                OldValueMembers.Add(oldValueMember);
+                OldValueProperties.Add(propertyInfo);
 
             }
             else
@@ -62,7 +124,14 @@ class ContractOldValueVisitor : ExpressionVisitor
 class DbcDefVisitor : ExpressionVisitor
 {
     private IList<ParameterExpression>? _contractParameters;
+    private readonly Type _contractType;
 
+    public readonly Dictionary<PropertyInfo, Delegate> OldValueCollectors = new();
+
+    public DbcDefVisitor(Type contractType)
+    {
+        _contractType = contractType;
+    }
     public override Expression? Visit(Expression? node)
     {
         if (_contractParameters == null && node is LambdaExpression lambda)
@@ -98,9 +167,24 @@ class DbcDefVisitor : ExpressionVisitor
                 var oldValueVisitor = new ContractOldValueVisitor();
                 oldValueVisitor.Visit(contractBody);
 
+                foreach (var oldValueMember in oldValueVisitor.OldValueProperties)
+                {
+                    if (!OldValueCollectors.ContainsKey(oldValueMember))
+                    {
+                        //var thisExpr = _contractParameters![0];
+                        var thisParamExpr = Expression.Parameter(_contractType, "thisContract");
+                        var collector = Expression.Lambda(Expression.Property(thisParamExpr, oldValueMember), thisParamExpr);
+                        var dlg = collector.Compile();
+
+                        OldValueCollectors.Add(oldValueMember, dlg);
+                    }
+                }
+
 
             }
         }
+
+        OldValueCollectors.Dump("old value collectors");
         return base.VisitMethodCall(node);
     }
 
